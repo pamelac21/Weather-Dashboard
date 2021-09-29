6b9035844c24d6be234a2c091b0814a7cdf020a2
PamelaCleveland <pamelac021@gmail.com>
2021-09-29 02:14:46 -0400

    current weather

--- assets/script.js
+++ assets/script.js
@@ -30,7 +30,9 @@ fetch(coordUrl)
   let latitude = response[0].lat;
   let longitude = response[0].lon;
   console.log(latitude, longitude)
-
+// city name
+$('#weather').html('')
+$('#weather').append(`<h2>${response[0].name}</h2>`)
 })
 
 let onecallUrl = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&apikey="+APIkey+"&units=imperial")
@@ -38,6 +40,19 @@ let onecallUrl = ("https://api.openweathermap.org/data/2.5/onecall?lat=" + latit
   .then((response) => response.json())
   .then(async (response) => {
       console.log(response)
+
+//current weather
+    let icon = ("https://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png")
+
+    $('#weather').append(`
+        <h3>${new Date(Date.now()).toLocaleString().split(',')[0]}
+        <img src="${icon}"></h2>
+        <ul class="list-unstyled">
+            <li>Temperature: ${response.current.temp}&#8457;</li>
+            <li>Humidity: ${response.current.humidity}%</li>
+            <li>Wind Speed: ${response.current.wind_speed}mph</li>
+            <li id="uvIndex"></li>
+        </ul>`)
   })
 }
 
