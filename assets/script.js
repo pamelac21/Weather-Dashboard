d4c0d621935eff19c0324e8ee4747b391eea4e33
PamelaCleveland <pamelac021@gmail.com>
2021-09-29 10:28:29 -0400

    searched cities render

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const APIkey = "77d964daadf18f4da0191f491935d9c4"
-let city = ""
+let city = ''
+let currentCity = ''
 
 
 
@@ -19,6 +20,7 @@ fetch(cityUrl)
 .then((response) => response.json())
 .then((response) => {
     console.log(response)
+    saveCity(city)
 })
 
 //coord fetch
@@ -96,9 +98,61 @@ $("#forecast").html(cardContent)
 }) 
 })}
 
+//localStorage
+let saveCity = (newCity) => {
+    let cityAlreadySaved = false
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage["cities" + i] === newCity) {
+        cityAlreadySaved = true
+        break;
+      }
+    }
+  
+    if (cityAlreadySaved === false) {
+      localStorage.setItem("cities" + localStorage.length, newCity);
+      console.log(localStorage)
+    }
+  }
+
+//display searched cities
+let searches = () => {
+    $("#searchResults").empty()
+    let lastCity = ''
+
+    if (localStorage.length === 0) {
+      if (lastCity) {
+        $("#searchBar").attr("value", lastCity)
+      } else {
+        $("#searchBar").attr("value", "Charlotte")
+      }
+    } else {
+      let lastCityKey = "cities" + (localStorage.length - 1)
+      lastCity = localStorage.getItem(lastCityKey)
+      $("#searchBar").attr("value", lastCity)
+      for (let i = 0; i < localStorage.length; i++) {
+        let city = localStorage.getItem("cities" + i)
+        let cityBtns;
+  
+        if (currentCity === '') {
+          currentCity = lastCity
+        }
+  
+        if (city === currentCity) {
+            cityBtns = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`
+          } else {
+            cityBtns = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`
+          }
+  
+        $("#searchResults").prepend(cityBtns)
+    }
+
+}
+}
 //search
 $("#searchBtn").on("click", (event) => {
     event.preventDefault()
 
     weather()
 })
+
+searches()
